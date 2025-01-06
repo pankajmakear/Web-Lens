e01dd99ad9a7bee6079c3486264202e378676903
pankajmakear <pankaj.p@makear.ai>
2025-01-06 12:47:15 +0530

    v4

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,72 @@
-import { bootstrapCameraKit } from '@snap/camera-kit';
+import {
+  bootstrapCameraKit,
+  CameraKitSession,
+  createMediaStreamSource,
+  Transform2D,
+} from '@snap/camera-kit';
 
-(async function () {
+const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement;
+const flipCamera = document.getElementById('flip') as HTMLButtonElement;
 
-  const apiToken = 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzMzMzc1OTY3LCJzdWIiOiIyM2U5NWFlZS0yNTRlLTRhYmYtYTk2MS1lNzBhOTNhZjRiM2N-U1RBR0lOR35jZTc4MmNjNC0zOGEwLTQ3YmUtOTU5NC00MjE4YWJlYjZkMzYifQ.5GMvAjVQ8v2dmDhps1OWei4rxn0Ba0jCnEhth9M7RiA'
-  const cameraKit = await bootstrapCameraKit({apiToken});
+let isBackFacing = true;
+let mediaStream: MediaStream;
 
-  const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement;
-const session = await cameraKit.createSession({ liveRenderTarget });
+async function init() {
+  const cameraKit = await bootstrapCameraKit({
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzMzMzc1OTY3LCJzdWIiOiIyM2U5NWFlZS0yNTRlLTRhYmYtYTk2MS1lNzBhOTNhZjRiM2N-U1RBR0lOR35jZTc4MmNjNC0zOGEwLTQ3YmUtOTU5NC00MjE4YWJlYjZkMzYifQ.5GMvAjVQ8v2dmDhps1OWei4rxn0Ba0jCnEhth9M7RiA',
+  });
 
-const mediaStream = await navigator.mediaDevices.getUserMedia({
-  video: true,
-});
+  const session = await cameraKit.createSession({ liveRenderTarget });
+  const lens = await cameraKit.lensRepository.loadLens(
+    '43300180875',
+    'ba31da6f-8bda-4b19-9ea7-ca7ff23e9931'
+  );
 
-await session.setSource(mediaStream);
+  session.applyLens(lens);
 
-await session.play();
+  bindFlipCamera(session);
+}
 
-const lens = await cameraKit.lensRepository.loadLens(
-  // App id and Group id
-  '50507980875',
-  'ba31da6f-8bda-4b19-9ea7-ca7ff23e9931'
-);
+function bindFlipCamera(session: CameraKitSession) {
+  flipCamera.style.cursor = 'pointer';
 
-await session.applyLens(lens);
+  flipCamera.addEventListener('click', () => {
+    updateCamera(session);
+  });
 
+  updateCamera(session);
+}
 
-})();
+async function updateCamera(session: CameraKitSession) {
+  isBackFacing = !isBackFacing;
+
+  flipCamera.innerText = isBackFacing
+    ? 'Switch to Front Camera'
+    : 'Switch to Back Camera';
+
+  if (mediaStream) {
+    session.pause();
+    mediaStream.getVideoTracks()[0].stop();
+  }
+
+  mediaStream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: isBackFacing ? 'environment' : 'user',
+    },
+  });
+
+  const source = createMediaStreamSource(mediaStream, {
+    // NOTE: This is important for world facing experiences
+    cameraType: isBackFacing ? 'environment' : 'user',
+  });
+
+  await session.setSource(source);
+
+  if (!isBackFacing) {
+    source.setTransform(Transform2D.MirrorX);
+  }
+
+  session.play();
+}
+
+init();
